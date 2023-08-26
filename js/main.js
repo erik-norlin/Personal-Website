d64bb7b1ee788059b1fce8e78a870b6194940ac9
eriknorlin <109854137+erik-norlin@users.noreply.github.com>
2023-08-26 12:18:11 +0200

    menu bar now working

--- js/main.js
+++ js/main.js
@@ -57,10 +57,16 @@
         
         if (!(hdr && hero)) return;
 
+        
         setTimeout(function(){
             triggerHeight = hero.offsetHeight - 170;
         }, 300);
+        
+        hdr.classList.add('sticky');
+        hdr.classList.add('offset');
+        hdr.classList.add('scrolling');
 
+        /*
         window.addEventListener('scroll', function () {
 
             let loc = window.scrollY;
@@ -69,23 +75,24 @@
             if (loc > triggerHeight) {
                 hdr.classList.add('sticky');
             } else {
-                hdr.classList.add('sticky'); /* .remove for disappearing effect */
+                hdr.classList.remove('sticky');
             }
 
             if (loc > triggerHeight + 20) {
                 hdr.classList.add('offset');
             } else {
-                hdr.classList.add('offset'); /* .remove for disappearing effect */
+                hdr.classList.remove('offset');
             }
 
             if (loc > triggerHeight + 150) {
                 hdr.classList.add('scrolling');
             } else {
-                hdr.classList.add('scrolling'); /* .remove for disappearing effect */
+                hdr.classList.remove('scrolling');
             }
 
         });
-
+        */
+        
     }; // end ssMoveHeader
 
 
